b490173cd66cc9af5208f4031431ae43b99e89b5
Gaik Tamazian <gaik.tamazian@gmail.com>
2014-11-20 23:57:28 +0300

    Transformation cost gradient added

---
+++ prompt-m/trmobjfunc.m
@@ -0,0 +1,118 @@
+function [F, G] = trmobjfunc(trmodel,indices,angles)
+%TRMOBJFUNC Calculate the cost function and its gradient for the model.
+%   trmobjfunc(trmodel,indices,angles) returns the cost of the specified
+%   transformation trmodel for values of torsion angles (angles) which
+%   indices are specified in the corresponding argument (indices).
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+trmodel.psi(indices,2:end-1) = ...
+    reshape(angles, length(indices), size(trmodel.psi, 2) - 2);
+
+[F, X] = trmcost(trmodel, 2);
+G = g(trmodel, X);
+G = G(indices, 2:end-1);
+
+end
+
+function G = g(model,X)
+%G Calculate the transformation cost gradient vector.
+%   g(model,X) returns the gradient vector of the transformation. It uses a
+%   precalculated matrix of Cartesian coordinates X of transformation atoms
+%   for better performance.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+D = d(X);
+
+n = size(model.r, 1) + 1;
+m = size(model.r, 2);
+
+G = zeros(n-3, m);
+
+for j = 2:m-1
+    H = 2*X{j} - X{j+1} - X{j-1};
+    for i = 1:n-3
+        G(i,j) = sum(2*model.m .* sum(H .* D{j,i}, 2));
+    end
+end
+
+end
+
+function D = d(X)
+%D Calculate values for a transformation cost gradient vector.
+%   d(X) calculates values for a transformation cost gradient vector based
+%   on a matrix of transformation atom Cartesian coordinates.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+m = length(X); % the number of configurations
+n = size(X{1}, 1); % the number of atoms
+
+U = u(X);
+D = cell(m,n-3);
+
+mask = [zeros(3,n-3); tril(ones(n-3,n-3))];
+
+for j = 1:m
+    for i = 1:n-3
+        D{j,i} = cross3dvec(repmat(U{j}(i,:), n, 1), ...
+            X{j} - repmat(X{j}(i+1,:), n, 1));
+        D{j,i} = D{j,i} .* repmat(mask(:,i), 1, 3);
+        D{j,i} = D{j,i} - repmat(sum( ...
+            cross3dvec(repmat(U{j}(i,:), n-i-2, 1), X{j}(i+3:end,:) - ...
+            repmat(X{j}(i+1,:), n-i-2, 1)), 1)/n, n, 1);
+    end
+end
+
+end
+
+function U = u(X)
+%U Calculate values for a transformation cost gradient vector.
+%   u(X) calculates values for a transformation cost gradient vector based
+%   on a matrix of transformation atom Cartesian coordinates.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+m = length(X); % the number of configurations
+n = size(X{1}, 1); % the number of atoms
+
+U = cell(1, m);
+for i = 1:m
+    U{i} = X{i}(3:n-1,:) - X{i}(2:n-2,:);
+    normU = repmat(sqrt(sum(U{i}.^2, 2)), 1, 3);
+    U{i} = U{i} ./ normU;
+end
+
+end
+
+function c = cross3dvec(a,b)
+%CROSS3DVEC Calculate cross products for matrix rows.
+%   cross3dvec(a,b) returns a matrix of cross products for rows of the
+%   specified matrices.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+c = [a(:,2).*b(:,3)-a(:,3).*b(:,2)
+    a(:,3).*b(:,1)-a(:,1).*b(:,3)
+    a(:,1).*b(:,2)-a(:,2).*b(:,1)];
+
+c = reshape(c, size(a));
+
+end
+
