bf9f6639d1a95eaef593467c6257eb1b79924d94
Gaik Tamazian <gaik.tamazian@gmail.com>
2014-11-21 19:01:58 +0300

    Function to detect interatomic clashes

---
+++ prompt-m/pdbmininteratomicdist.m
@@ -0,0 +1,21 @@
+function mindist = pdbmininteratomicdist(PDBStruct)
+%PDBMININTERATOMICDIST Returns the minimal interatomic distances.
+%   pdbmininteratomicdist(PDBStruct) returns the vector of the minimal
+%	interatomic distances between the atoms that belong to the same
+%	transformation confguration. Each element of the vector corresponds
+%	to the certain configuration.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+coords = pdbextractcoords(PDBStruct);
+mindist = length(coords);
+
+for i = 1:length(coords)
+	mindist(i) = min(pdist(coords{i}));
+end
+
+end
+
