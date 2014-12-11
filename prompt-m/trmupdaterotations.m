6b23a8eda0c4dd8529840097b3d5b3e3921cb3fc
Gaik Tamazian <gaik.tamazian@gmail.com>
2014-12-11 15:40:47 +0300

    Function to update rotation matrices

---
+++ prompt-m/trmupdaterotations.m
@@ -0,0 +1,26 @@
+function result = trmupdaterotations(trmodel)
+%TRMUPDATEROTATIONS Update rotation matrices using the Kabsch algorithm.
+%   trmupdaterotations(trmodel) updates rotation matrices assigned to each
+%   configuration of the specified transformation using the Kabsch
+%   algorithm.
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2014.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+prevCoords = trmodel.StartCoords;
+nModels = length(trmodel.U);
+
+for j = 2:nModels
+    currCoords = restorecoords(trmodel.r(:,j), trmodel.alpha(:,j), ...
+        trmodel.psi(:,j));
+    q = optimquat(prevCoords,currCoords);
+    trmodel.U{j} = quat2rotmat(q);
+    prevCoords = currCoords;
+end
+
+result = trmodel;
+
+end
+
