83e3370b59e3be2ca6d2bcd37073539cad36ee16
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-04-11 17:32:06 +0300

    Function to compare transformations added

---
+++ pdbplottrfcmp.m
@@ -0,0 +1,31 @@
+function [h, rmsdValues] = pdbplottrfcmp(pdbStruct1, pdbStruct2)
+%PDBPLOTTRFCMP Plot RMSDs between configurations of the PDB structures.
+%   PDBPLOTTRFCMP(pdbStruct1, pdbStruct2) plots RMSDs between
+%   configurations of the specifed PDB structures representing a protein
+%   transformation. It also returns the RMSD values as an optional output
+%   argument.
+%
+%   See also pdbplotadjrmsd pdbplotfixedrmsd
+%
+% PROMPT Toolbox for MATLAB
+
+% By Gaik Tamazian, 2015.
+% gaik (dot) tamazian (at) gmail (dot) com
+
+nModels = length(pdbStruct1.Model);
+rmsdValues = zeros(nModels, 1);
+coords1 = pdbextractcoords(pdbStruct1);
+coords2 = pdbextractcoords(pdbStruct2);
+for i = 1:nModels
+    % superpose points of the second model to points of the first one
+    [~, coords2{i}] = procrustes(coords1{i}, coords2{i}, ...
+        'scaling', false, 'reflection', false);
+    rmsdValues(i) = mean(sqrt(sum((coords1{i} - coords2{i}).^2, 2)));
+end
+
+h = plot(transpose(rmsdValues), '-o');
+xlabel('Configuration Number');
+ylabel('RMSD in AA');
+
+end
+
