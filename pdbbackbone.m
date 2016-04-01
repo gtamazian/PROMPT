0794a1fbf2505535e5bbf47d9584cc23d05e24c6
Gaik Tamazian <gaik.tamazian@gmail.com>
2016-04-02 00:49:31 +0300

    Option to leave only alpha carbon atoms

--- pdbbackbone.m
+++ pdbbackbone.m
@@ -1,7 +1,9 @@
-function backbonePDBStruct = pdbbackbone(PDBStruct)
+function backbonePDBStruct = pdbbackbone(PDBStruct, onlyCA)
 %PDBBACKBONE Return a PDB structure only with backbone atoms
-%   PDBBACKBONE(PDBStruct) returns a PDB structure that contains only
-%   backbone atoms (that is, atoms, which names are N, C, and CA).
+%   PDBBACKBONE(PDBStruct, onlyCA) returns a PDB structure that contains 
+%   only backbone atoms (that is, atoms, which names are N, C, and CA) or
+%   only alpha carbon atoms if then onlyCA option is set to true. By
+%   default, onlyCA is false.
 %
 %   See also pdbextractcoords
 %
@@ -10,12 +12,22 @@ function backbonePDBStruct = pdbbackbone(PDBStruct)
 % By Gaik Tamazian, 2014.
 % gaik (dot) tamazian (at) gmail (dot) com
 
+if nargin < 2
+    onlyCA = false;
+end
+
+if onlyCA
+    atom_names = {'CA'};
+else
+    atom_names = {'N', 'C', 'CA'};
+end
+
 backbonePDBStruct = PDBStruct;
 nModels = length(PDBStruct.Model);
 for iModel = 1:nModels
     backbonePDBStruct.Model(iModel).Atom = ...
         PDBStruct.Model(iModel).Atom(ismember(...
-        {PDBStruct.Model(iModel).Atom.AtomName}, {'N' 'C' 'CA'}));
+        {PDBStruct.Model(iModel).Atom.AtomName}, atom_names));
     backbonePDBStruct.Model(iModel).HeterogenAtom = [];
 end
 
