ca101533f8e2eb1c06af675eb01707bf1dd27580
Gaik Tamazian <gaik.tamazian@gmail.com>
2016-05-09 17:28:42 +0300

    The cross function optimized

--- prompt.f90
+++ prompt.f90
@@ -36,7 +36,9 @@ contains
     integer(kind=4) :: i
 
     do i = 1, n
-      c(i, :) = cross3d(a(i, :), b(i, :))
+      c(i, 1) = a(i, 2) * b(i, 3) - a(i, 3) * b(i, 2)
+      c(i, 2) = a(i, 3) * b(i, 1) - a(i, 1) * b(i, 3)
+      c(i, 3) = a(i, 1) * b(i, 2) - a(i, 2) * b(i, 1)
     end do
 
   end function cross
