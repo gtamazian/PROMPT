9fb6dcc27d58145f6f81c8b4fe1c7f87744d6458
Gaik Tamazian <mail@gtamazian.com>
2016-06-10 19:28:25 +0300

    trmiteroptim parameters modified

--- trmiteroptim.m
+++ trmiteroptim.m
@@ -1,16 +1,14 @@
 function [models, optimResults] = trmiteroptim(trmodel, nPlanar, ...
-    nTorsion, iterSteps, optimOptions)
+    nTorsion, optimOptions, nIterations)
 %TRMITEROPTIM Iterative optimization scheme.
-%   TRMITEROPTIM(trmodel, nPlanar, nTorsion, iterSteps, optimOptions)
-%   implements an iterative optimization scheme; the iterSteps array
-%   specifies the number of iterations for each stage; nPlanar and nTorsion
+%   TRMITEROPTIM(trmodel, nPlanar, nTorsion, optimOptions, nIterations)
+%   implements an iterative optimization scheme; nPlanar and nTorsion
 %   specify the numbers of planar and torsion angles to be optimized;
-%   optimOptions is the structure of the optimization process parameters.
-%   The optimization scheme stops if the current stage have not modified
-%   the initial point. Also one may specify an empty list as the iterSteps
-%   value; in this case, the optimization will continue until local minima
-%   are reached at each stage and the current local minimum differs from
-%   the ones obtained at the previous stage.
+%   optimOptions is the structure of the optimization process parameters;
+%   nIterations is the maximum number of optimization stages (the default
+%   values is Inf). The optimization scheme stops if the current stage
+%   have not modified the initial point or the number of stages is greater
+%   tha  the nIterations value.
 %
 %   See also trmobjfunc
 %
@@ -19,9 +17,12 @@ function [models, optimResults] = trmiteroptim(trmodel, nPlanar, ...
 % By Gaik Tamazian, 2016.
 % mail (at) gtamazian (dot) com
 
-nSteps = length(iterSteps);
-models = cell(1, nSteps);
-optimResults = cell(1, nSteps);
+if nargin < 5
+    nIterations = Inf;
+end
+
+models = cell(1, 1000);
+optimResults = cell(1, 1000);
 
 % first, determine indices of planar and torsion angles to be optimized
 P = trmdistantangleindices(trmodel, nPlanar, 'planar');
@@ -29,38 +30,28 @@ T = trmdistantangleindices(trmodel, nTorsion, 'torsion');
 
 iStep = 1;
 while 1
-    currOptimOptions = optimOptions;
-    if nSteps > 0
-        currOptimOptions.MaxIterations = iterSteps(iStep);
-    else
-        currOptimOptions.MaxIterations = Inf;
-    end
-    
     % get the objective function for the current stage
     f = @(x) trmobjfunc(trmodel, P, T, x);
     
     % get the initial point
     initial_point = trminitialpoint(trmodel, P, T);
     
-    [x,~,~,output] = fmincon(f, initial_point, [], [], [], [], ...
-        -pi*ones(size(initial_point)), pi*ones(size(initial_point)), ...
-        [], currOptimOptions);
+    problem.options = optimOptions;
+    problem.solver = 'fminunc';
+    problem.x0 = initial_point;
+    problem.objective = f;
+
+    [x,~,~,output] = fminunc(problem);
     
     trmodel = trmchangeangles(trmodel, P, T, x);
     models{iStep} = trmodel;
     optimResults{iStep} = output;
     
     trmodel = trmupdaterotations(trmodel);
-    
-    % check if we should stop the process because the number of steps is
-    % equal to the specified one
-    if (nSteps > 0) && (iStep == nSteps)
-        break
-    end
-    
+
     % check if we should stop the process because we have reached the local
     % minimum
-    if (iStep > 0) && (output.iterations < 2)
+    if ((iStep > 0) && (output.iterations < 2)) || (iStep == nIterations)
         models = models(1:iStep);
         optimResults = optimResults(1:iStep);
         break
